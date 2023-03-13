7a02d04080013e2f94fd67d3af92f72c03f35ccf
Fadhil Muhammad <fadhil.setiawan@tech.jago.com>
2023-03-13 14:40:33 +0700

    :zap: Adding filetree

--- nvim/lua/custom/plugins/filetree.lua
+++ nvim/lua/custom/plugins/filetree.lua
@@ -1,5 +1,6 @@
 -- Unless you are still migrating, remove the deprecated commands from v1.x
 vim.cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
+vim.keymap.set('n', '<Bslash>', ':Neotree<CR>')
 
 return {
 	'nvim-neo-tree/neo-tree.nvim',
