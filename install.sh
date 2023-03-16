2540afab7fee88cf72701e48ab75fd9ffdd9e47c
Fadhil Muhammad <fadhil.setiawan@tech.jago.com>
2023-03-17 00:10:45 +0700

    :hammer: Removed `vim-plug` since `lazy` is awesome now

--- install.sh
+++ install.sh
@@ -37,11 +37,6 @@ installBrew() {
   brew bundle
 }
 
-installVimPlug() {
-  sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-}
-
 installXcode() {
   if ! xcode-select --print-path &> /dev/null; then
     xcode-select --install &> /dev/null
