d5d950d67ecd2f06e4b855fa1d5342de15724019
Fadhil Muhammad <fadhil.setiawan@tech.jago.com>
2023-07-01 17:38:18 +0700

    :hammer: Add dependencies into indent blankline

--- nvim/lua/repo/indent_blankline.lua
+++ nvim/lua/repo/indent_blankline.lua
@@ -1,5 +1,10 @@
 return {
   'lukas-reineke/indent-blankline.nvim',
+  dependencies = {
+    'tpope/vim-fugitive',
+    'tpope/vim-sleuth',
+    'tpope/vim-surround',
+  },
   init = function()
     require('utils.helpers').lazy_load 'indent-blankline.nvim'
   end,
