34b0c6a6b9176f7ee778dba705539cad578c648a
Fadhil Muhammad <fadhil.setiawan@tech.jago.com>
2024-09-18 23:52:07 +0700

    :zap: mason now supports `ts_ls`

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -28,7 +28,7 @@ end
 
 local servers = {
   kotlin_language_server = {},
-  tsserver = {},
+  ts_ls = {},
   cssls = {},
   dockerls = {},
   jsonls = {},
@@ -83,10 +83,6 @@ mason_lspconfig.setup {
 
 mason_lspconfig.setup_handlers {
   function(server_name)
-    if server_name == 'tsserver' then
-      server_name = 'ts_ls'
-    end
-
     require('lspconfig')[server_name].setup {
       capabilities = capabilities,
       on_attach = on_attach,
