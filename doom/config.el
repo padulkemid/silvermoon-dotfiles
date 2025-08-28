5aa4c2e86c0f3d96f319ff5fc9ed3e708e049c80
Fadhil Muhammad <bringitdown0@gmail.com>
2025-08-28 17:02:46 +0700

    :hammer: forgot to add the space

--- doom/config.el
+++ doom/config.el
@@ -146,7 +146,7 @@
                                             '(vc-mode vc-mode)
                                             " "
                                             mode-line-misc-info
-                                            " L%l:C%c "))))
+                                            " L%l:C%c  "))))
 
 (setq padul/mode-line-left-side '(" "
                                   (:eval (format-time-string "%H:%M"))
