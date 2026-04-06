ead63b3cfaf26fd3bb3e0d25340ca03226c591d0
Fadhil Muhammad <bringitdown0@gmail.com>
2026-04-06 09:42:36 +0700

    feat(disabled): disable defaults

---
+++ nvim/lua/disabled.lua
@@ -0,0 +1,36 @@
+local plugins = {
+  '2html_plugin',
+  '2html',
+  'tohtml',
+  'getscript',
+  'getscriptPlugin',
+  'gzip',
+  'logipat',
+  'netrw',
+  'netrwPlugin',
+  'netrwSettings',
+  'netrwFileHandlers',
+  'matchit',
+  'matchparen',
+  'tar',
+  'tarPlugin',
+  'rrhelper',
+  'spellfile',
+  'spellfile_plugin',
+  'vimball',
+  'vimballPlugin',
+  'zip',
+  'zipPlugin',
+  'tutor',
+  'rplugin',
+  'syntax',
+  'synmenu',
+  'optwin',
+  'compiler',
+  'bugreport',
+  'ftplugin',
+}
+
+for _, plugin in pairs(plugins) do
+  vim.g['loaded_' .. plugin] = 1
+end
