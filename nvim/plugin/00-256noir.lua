f7aba1fc23606f6ac7f2888c9bf03f5e87e7283b
Fadhil Muhammad <bringitdown0@gmail.com>
2026-05-08 01:25:21 +0700

    :hammer: forgot to comment

--- nvim/plugin/00-256noir.lua
+++ nvim/plugin/00-256noir.lua
@@ -2,5 +2,5 @@ vim.pack.add {
   '/Users/padulkemid/Work/personal-projects/nvim-plugins/nvim-256noir'
 }
 
-vim.cmd.colorscheme('256noir')
+-- vim.cmd.colorscheme('256noir')
 vim.cmd.colorscheme('delek')
