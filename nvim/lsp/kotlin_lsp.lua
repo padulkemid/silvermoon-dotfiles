e8b3d2fdb5741eed6d50023ae1f3aae7aa2d291d
Fadhil Muhammad <bringitdown0@gmail.com>
2026-09-03 07:09:47 +0700

    :hammer: add kotlin_lsp decompiler for stdlib, plugins, etc.

--- nvim/lsp/kotlin_lsp.lua
+++ nvim/lsp/kotlin_lsp.lua
@@ -1,27 +1,6 @@
---[[ local function get_mise_java(version)
-  local cmd = version and ('mise where java@' .. version) or 'mise where java'
-  local handle = io.popen(cmd .. ' 2>/dev/null')
-  if not handle then
-    return nil
-  end
-  local path = handle:read '*l'
-  handle:close()
-  return path
-end
-
-local java_21 = get_mise_java '21' ]]
-
--- sometimes using custom gradle wouldn't work because it matches to JBR 25
--- just use intellij for kotlin, for now.
--- nvim is good for fast viewing code/diff
 return {
   filetypes = { 'kotlin' },
   cmd = { 'intellij-server', '--stdio' },
-  --[[ settings = {
-    intellij = {
-      jdkForSymbolResolution = java_21,
-    },
-  }, ]]
   root_markers = {
     'settings.gradle',
     'settings.gradle.kts',
